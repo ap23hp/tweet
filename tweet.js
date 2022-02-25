80e26af1df3870417c6eb093b2b5f4b52b54cd93
ap23hp <67959896+ap23hp@users.noreply.github.com>
2022-02-25 19:01:21 +0530

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -13,6 +13,11 @@ updateCount(tweet)
 function updateCount(tweet){
     console.log("counting",tweet)
     num.innerText=`${tweet.length}/20`
+     window.localStorage.setItem("tweet",tweet)
 }
 
+const storedTweet=window.localStorage.getItem("tweet")
+console.log(storedTweet)
+document.querySelector(".tweet").innerText=storedTweet
+updateCount(storedTweet)
 
